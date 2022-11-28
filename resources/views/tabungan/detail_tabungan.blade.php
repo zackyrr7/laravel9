97b6ae8bb432e7d606cd1b3f7ebcec5bf4e3f5ce
bimaarf <bimaarifa5@gmail.com>
2022-11-29 00:55:56 +0700

    auth -- role -- relations

--- resources/views/tabungan/detail_tabungan.blade.php
+++ resources/views/tabungan/detail_tabungan.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.backend.main')
 
 @section('content')
-<div class="container-fluid">
+    <div class="container-fluid">
 
         <!-- Page Heading -->
         <h1 class="h3 mb-4 text-gray-800">detail User</h1>
@@ -10,15 +10,15 @@
                 <!-- Dropdown Card Example -->
                 <div class="card shadow mb-4">
                     <!-- Card Header - Dropdown -->
-                   
+
                     <!-- Card Body -->
                     <div class="card-body">
-                    Nama: {{ $tabungan->name ?? 'None' }} <br/>
-                    Nomor Hp: {{ $user->email ?? 'None' }}<br/>
+                        Nama: {{ $tabungan->name ?? 'None' }} <br />
+                        Nomor Hp: {{ $user->email ?? 'None' }}<br />
                     </div>
-    
-                    
+
                 </div>
+                <a href="{{ route('tamb') }}" class="btn btn-success float-right">Tambah Tabungan</a>
             </div>
         </div>
     </div>
